39c98812b0f9ba3ff865370c0116a95882fbda40
Nathan Da Silva <117369146+Nathandasiilva@users.noreply.github.com>
2023-06-13 14:16:13 -0300

    update

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+   const html = document.documentElement
+    
+   html.classList.toggle('light')
+    //if(html. classList.contains('light')) {
+    //    html.classList.remove('light')
+    //}else {
+    //    html.classList.add('light')
+    //}
+
+    //Pegando a img
+
+    const img = document.querySelector("#profile img")
+
+    //if(html.classList.contains('light')) {
+    //Substituindo a img
+    //img.setAttribute('src','/imagens/')
+    //}
+    //else {
+    //Se tiver sem o light mode, manter a imagem normal
+    //img.setAttribute('src', './imagens/foto_personalizada.png')
+    //}
+
+}
